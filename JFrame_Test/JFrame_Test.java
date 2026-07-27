ac22ff0a6620bbca07ef9c6c5c9f08a2f21be5d7
xGp35 <girijaprasanna1999@gmail.com>
2026-07-28 01:14:56 +0530

    stated simple gui

--- JFrame_Test/JFrame_Test.java
+++ JFrame_Test/JFrame_Test.java
@@ -1,1 +1,11 @@
+// Make a Frame
 JFrame frame = new JFrame();
+
+// Make a widget
+JButton button = new JButton("click me");
+
+//Add the widget to the Frame
+frame.getContentPane().add(button);
+
+frame.setSize(300,300);
+frame.setVisible(true);
