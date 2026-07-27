015b327e34881e9b666692be9b49daf34b0e9105
Girija Prasanna <girijaprasanna1999@gmail.com>
2026-07-28 01:21:18 +0530

    simple GUI

--- JFrame_Test/SimpleGui1.java
+++ JFrame_Test/SimpleGui1.java
@@ -1,1 +1,17 @@
 import javax.swing.*;
+
+public class SimpleGui1 {
+    public static void main(String[] args) {
+        
+        JFrame frame = new JFrame();
+        JButton button = new JButton("click me");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Makes the program quit as soon as you close the window
+        // Without this, it will just sit there on the screen till shutdown.
+
+        frame.getContentPane().add(button);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
+}
