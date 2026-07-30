64aa4578cc9dbba28bb6d54abfdaea79e6599602
Girija Prasanna <girijaprasanna1999@gmail.com>
2026-07-30 17:30:30 +0530

    Add JFrame_Test class and MyDrawPanel for custom painting

--- JFrame_Test/JFrame_Test.java
+++ JFrame_Test/JFrame_Test.java
@@ -1,11 +1,22 @@
-// Make a Frame
-JFrame frame = new JFrame();
+    import javax.swing.*;
 
-// Make a widget
-JButton button = new JButton("click me");
+    public class JFrame_Test {
+        public static void main(String[] args) {
+            
+            // Make a Frame
+            JFrame frame = new JFrame();
 
-//Add the widget to the Frame
-frame.getContentPane().add(button);
+            // Make a widget
+            JPanel myPanel = new MyDrawPanel();
 
-frame.setSize(300,300);
-frame.setVisible(true);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            // Makes the program quit as soon as you close the window
+            // Without this, it will just sit there on the screen till shutdown.
+
+            //Add the widget to the Frame
+            frame.getContentPane().add(myPanel);
+
+            frame.setSize(300,300);
+            frame.setVisible(true);
+        }
+    }
