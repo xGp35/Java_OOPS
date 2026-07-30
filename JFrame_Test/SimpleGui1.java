c5afe2af7259eba50da79b015971f4c32d779fe8
Girija Prasanna <girijaprasanna1999@gmail.com>
2026-07-30 15:04:54 +0530

    Add radio button and checkbox to SimpleGui1

--- JFrame_Test/SimpleGui1.java
+++ JFrame_Test/SimpleGui1.java
@@ -5,12 +5,19 @@ public class SimpleGui1 {
         
         JFrame frame = new JFrame();
         JButton button = new JButton("click me");
+        JRadioButton radioButton = new JRadioButton("choose me");
+        JCheckBox checkBox = new JCheckBox("check me out");
+        JLabel label = new JLabel("label me");
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         // Makes the program quit as soon as you close the window
         // Without this, it will just sit there on the screen till shutdown.
 
         frame.getContentPane().add(button);
+        //frame.getContentPane().add(radioButton);
+        //frame.getContentPane().add(checkBox);
+        frame.getContentPane().add(label);
+    
         frame.setSize(300, 300);
         frame.setVisible(true);
     }
