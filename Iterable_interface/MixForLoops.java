6daecb80159a9b6d4f864a402b1ea6f9884df139
Girija Prasanna <girijaprasanna1999@gmail.com>
2026-07-31 15:20:26 +0530

    partial organising the numbers in the folder names

--- Iterable_interface/MixForLoops.java
+++ Iterable_interface/MixForLoops.java
@@ -4,8 +4,10 @@ class MixForLoops {
     public static void main(String [] args) {
         List<Integer> nums = List.of(1, 2, 3, 4, 5);
         String output = "";
-        for (int i = 0; i <= nums.size(); i++)
-        output += nums.get(i) + " ";
-        System.out.println(output);
+        for (int i = 0; i <= nums.size(); i++) {
+            output += nums.get(i) + " ";
+            System.out.println(output);
+        }
+        
     }
 }
