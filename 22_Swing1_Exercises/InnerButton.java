63b57d91876aed2d920a44757f99182103935c4e
Girija Prasanna <girijaprasanna1999@gmail.com>
2026-07-31 21:21:58 +0530

    Swing exercises - A, B switcher and shrinking rectangle

---
+++ 22_Swing1_Exercises/InnerButton.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class InnerButton {
+    private JButton button;
+
+    public static void main(String[] args) {
+        InnerButton gui = new InnerButton();
+        gui.go();
+    }
+
+    public void go() {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        button = new JButton("A");
+        button.addActionListener(new ButtonListener()); // This must have a class that implements ActionListener
+
+        frame.getContentPane().add(BorderLayout.SOUTH, button);
+        frame.setSize(200, 100);
+        frame.setVisible(true);
+    }
+
+    class ButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            if (button.getText().equals("A")) {
+                button.setText("B");
+            } else {
+                button.setText("A");
+            }
+        }
+    }
+
+}
